4011cda75e5cfdd1100bc2c1f1275fd7a16d078f
chandrasagarjain <chandrasagarjain@gmail.com>
2018-09-14 11:36:43 +0530

    Update paymentSupport.js

--- paymentSupport.js
+++ paymentSupport.js
@@ -1,2 +1,18 @@
 var mvp = document.getElementById('myViewport');
 mvp.setAttribute('content','width=device-width,initial-scale=0.4,minimum-scale=0.4,maximum-scale=0.4,user-scalable=no');
+
+console.log("Script is running ::::::::::::::::::::::CHEERS");
+console.log(":::::::::::::Body is " , document.body.innerText);
+window.onload = "loadFunction()";
+
+function loadFunction() { 
+    Console.log(":Window is loaded succesfully ::::::");
+    console.log("The nody is after loading is ::::::" , document.body.innerText);
+    if ( document.body.innerText.indexOf("Success message")) // Put the actual success message here 
+    {   console.log("Found the success message ");
+    } else { 
+        console.log("Did not find the success message , wait for user to proceed to following page before confirmation");
+        //unset global variable 
+
+    }
+}
