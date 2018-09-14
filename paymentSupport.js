c1cd107125ef9f92ab30f78a044060bef5d02d10
chandrasagarjain <chandrasagarjain@gmail.com>
2018-09-14 12:58:45 +0530

    Update paymentSupport.js

--- paymentSupport.js
+++ paymentSupport.js
@@ -1,7 +1,7 @@
 var mvp = document.getElementById('myViewport');
 mvp.setAttribute('content','width=device-width,initial-scale=0.4,minimum-scale=0.4,maximum-scale=0.4,user-scalable=no');
 
-console.log("Script is running ::::::::::::::::::::::");
+console.log("Script is running ::::::::::::::::::::::cHEERS");
 console.log(":::::::::::::Body is " , document.body.innerText);
 var index= document.body.innerText.indexOf("Payment Confirmation Number");
                                             
