fee256b15920c6cd548b32f9bdf686c48dc0233a
chandrasagarjain <chandrasagarjain@gmail.com>
2018-09-14 12:44:27 +0530

    Update paymentSupport.js

--- paymentSupport.js
+++ paymentSupport.js
@@ -1,7 +1,7 @@
 var mvp = document.getElementById('myViewport');
 mvp.setAttribute('content','width=device-width,initial-scale=0.4,minimum-scale=0.4,maximum-scale=0.4,user-scalable=no');
 
-console.log("Script is running ::::::::::::::::::::::CHEERS");
+console.log("Script is running ::::::::::::::::::::::");
 console.log(":::::::::::::Body is " , document.body.innerText);
 window.onload = "loadFunction()";
 
