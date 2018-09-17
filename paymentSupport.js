44e4634e39edf3ef1919f5bdaf822af54daa06e0
chandrasagarjain <chandrasagarjain@gmail.com>
2018-09-17 18:47:54 +0530

    Update paymentSupport.js
    Commit after confusion toward name change

--- paymentSupport.js
+++ paymentSupport.js
@@ -1,56 +1,7 @@
-/* For setting view mode settings */
 var mvp = document.getElementById('myViewport');
-mvp.setAttribute('content','viewport-fit=cover, width=device-width');
-
-/*Two URLs for invocation*/
-var ACH_URL = 'https://policycenter.aceprivateriskservices.com/billing/terms_ACH_Debits.cfm';
-var TNC_URL = 'https://www2.chubb.com/us-en/terms-of-use.aspx';
-
-/*For reducing width of button on 1A page One Time Bill Payment */
-if(document.getElementsByName("back")[0].value === "Back to payment information") {
-    document.getElementsByName("back")[0].value = "Back";
-}
-
-if(document.wizForm.action.value === "PaymentMethodBank") {
-/* For reducing horizontal scroll on Contact Info */
-        document.getElementsByName('areaCode')[0].size="5";
-        document.getElementsByName('part1')[0].size="5";
-        document.getElementsByName('part2')[0].size="7";
-
-        document.getElementsByName('contactInfo.email')[0].size="30";
-        document.getElementsByName('emailConf')[0].size="30";
-}
-
-/*For adding ACH FAQ Display Window */
-    if(document.wizForm.action.value === "ConfirmBank") {
-                    var c = document.createElement('input');
-                    c.name='button3';
-                    c.type='button';
-                    c.value='View ACH Terms';
-                    c.setAttribute("style","font-size: 2rem !important;width: auto;padding: 10px !important;margin-right: 40px;	border-bottom: thin solid #8B4500;border-right: thin solid #8B4500;border-top: thin solid #8B4500;border-left: thin solid #8B4500;font-weight: bold;background-color: #996633;color: #FFFFFF;margin: 2px");
-                    document.getElementsByClassName('actionbutton')[0].parentElement.appendChild(c);
-                    c.addEventListener("click",function(){location.href='https://cuemedocmanager/AceACH/;' + ACH_URL},false);
-    }
-/* For adding ACH TnC Display Window */        
-    else if(document.wizForm.action.value === "SubmitPayment") {
-                    var d = document.createElement('input');
-                    d.name='button4';
-                    d.type='button';
-                    d.value='View Terms';
-                    d.setAttribute("style","font-size: 2rem !important;width: auto;padding: 10px !important;margin-right: 40px;	border-bottom: thin solid #8B4500;border-right: thin solid #8B4500;border-top: thin solid #8B4500;border-left: thin solid #8B4500;font-weight: bold;background-color: #996633;color: #FFFFFF;margin: 2px");
-                    document.getElementsByClassName('actionbutton')[0].parentElement.appendChild(d);
-                    d.addEventListener("click",function(){location.href='https://cuemedocmanager/AceTNC/;' + TNC_URL});
-    }
-
-document.getElementsByName('areaCode')[0].value = 123;
-
-
-index = document.body.innerText.indexOf("Payment Request Submitted");
-console.log("Index is " , index);
-if( index == -1 ) { 
-    console.log("Found String");
-    location.href='https://cuemedocmanager/fisString/;'
-
-}
-
-
+mvp.setAttribute('content','width=device-width,initial-scale=0.4,minimum-scale=0.4,maximum-scale=0.4,user-scalable=no');	mvp.setAttribute('content','width=device-width,initial-scale=0.4,minimum-scale=0.4,maximum-scale=0.4,user-scalable=no');
+ var index= document.body.innerText.indexOf("Payment Confirmation Number");                                            	var indexConfirm= document.body.innerText.indexOf("Payment Confirmation Number"); 
+var indexFail= document.body.innerText.indexOf("An error occurred while creating Card transaction: We are unable to process your transaction at this time.");    
+ if( index !== -1 ) {	if( indexConfirm !== -1 ) {
+    location.href='https://cuemedocmanager/ConfirmString/;'	    location.href='https://cuemedocmanager/ConfirmString/;'
+}	}
