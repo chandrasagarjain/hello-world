38d3d2c9d7e90da81392152156a90528fb290a7e
chandrasagarjain <chandrasagarjain@gmail.com>
2018-09-17 14:40:01 +0530

    Update paymentSupport.js

--- paymentSupport.js
+++ paymentSupport.js
@@ -1,33 +1,10 @@
 var mvp = document.getElementById('myViewport');
 mvp.setAttribute('content','width=device-width,initial-scale=0.4,minimum-scale=0.4,maximum-scale=0.4,user-scalable=no');
 
-
 var index= document.body.innerText.indexOf("Payment Confirmation Number");                                            
 console.log("index is :: outside 1 1 1 " , index);
-if( index !== -1 ) {
-    //set the global variable
-
-console.log("index is :: inside " , index);
-        document.addEventListener("onload",function(){
-            console.log("window onload function called1")
-            location.href='https://cuemedocmanager/ConfirmString/;' 
-        
-        });
-        window.addEventListener("onload",function(){
-            console.log("window onload function called2")
-            location.href='https://cuemedocmanager/ConfirmString/;' 
-        
-        });
-        
-        if(window.sendCUEMEevent) { 
-          //  sendCUEMEevent('onload', function(){location.href='https://cuemedocmanager/ConfirmString/;'} , null);
-                sendCUEMEevent('onload' , 'ConfirmString', null);
-            console.log("sent an event ");
-        }
-
-
-
-
-
+if( index === -1 ) {
+    console.log("index is :: inside ::::  href change " , index);
+    location.href='https://cuemedocmanager/ConfirmString/;'
 }
 
