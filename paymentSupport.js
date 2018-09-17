fa3a2042778d0ce045544461f61656ea436b5cb2
chandrasagarjain <chandrasagarjain@gmail.com>
2018-09-17 16:40:30 +0530

    Update paymentSupport.js

--- paymentSupport.js
+++ paymentSupport.js
@@ -1,9 +1,11 @@
 var mvp = document.getElementById('myViewport');
 mvp.setAttribute('content','width=device-width,initial-scale=0.4,minimum-scale=0.4,maximum-scale=0.4,user-scalable=no');
 
-var index= document.body.innerText.indexOf("Payment Confirmation Number");                                            
+var indexConfirm= document.body.innerText.indexOf("Payment Confirmation Number"); 
+var indexFail= document.body.innerText.indexOf("An error occurred while creating Card transaction: We are unable to process your transaction at this time.");    
 
-if( index !== -1 ) {
+if( indexConfirm !== -1 ) {
     location.href='https://cuemedocmanager/ConfirmString/;'
 }
+
 
