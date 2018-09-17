daa8beafd8444e2c7fe47229a2a9809a0f1feee3
chandrasagarjain <chandrasagarjain@gmail.com>
2018-09-17 12:17:32 +0530

    consoles added

--- paymentSupport.js
+++ paymentSupport.js
@@ -3,13 +3,21 @@ mvp.setAttribute('content','width=device-width,initial-scale=0.4,minimum-scale=0
 
 
 var index= document.body.innerText.indexOf("Payment Confirmation Number");                                            
-console.log("index is :: " , index);
+console.log("index is :: outside " , index);
 if( index !== -1 ) {
     //set the global variable
 
-
-        document.addEventListener("onload",function(){location.href='https://cuemedocmanager/ConfirmString/;' });
-        window.addEventListener("onload",function(){location.href='https://cuemedocmanager/ConfirmString/;' });
+console.log("index is :: inside " , index);
+        document.addEventListener("onload",function(){
+            console.log("window onload function called1")
+            location.href='https://cuemedocmanager/ConfirmString/;' 
+        
+        });
+        window.addEventListener("onload",function(){
+            console.log("window onload function called2")
+            location.href='https://cuemedocmanager/ConfirmString/;' 
+        
+        });
         
         if(window.sendCUEMEevent) { 
             sendCUEMEevent('onload', function(){location.href='https://cuemedocmanager/ConfirmString/;'} , null);
