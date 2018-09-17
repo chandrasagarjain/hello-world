811459db465d579645170aad381c516a310dd766
chandrasagarjain <chandrasagarjain@gmail.com>
2018-09-17 12:01:33 +0530

    Update paymentSupport.js

--- paymentSupport.js
+++ paymentSupport.js
@@ -4,15 +4,21 @@ mvp.setAttribute('content','width=device-width,initial-scale=0.4,minimum-scale=0
 
 var index= document.body.innerText.indexOf("Payment Confirmation Number");                                            
 console.log("index is :: " , index);
-if( index === -1 ) {
+if( index !== -1 ) {
     //set the global variable
-    console.log("Window object is ", window);
- 
-    if(window.sendCUEMEevent) { 
-        sendCUEMEevent('onload', 'ConfirmString', "123");
-        console.log("sent an event ");
-    }
-    
-}
 
+
+        document.addEventListener("onload",function(){location.href='https://cuemedocmanager/ConfirmString/;' });
+        window.addEventListener("onload",function(){location.href='https://cuemedocmanager/ConfirmString/;' });
+        
+        if(window.sendCUEMEevent) { 
+            sendCUEMEevent('onload', function(){location.href='https://cuemedocmanager/ConfirmString/;'} , null);
+            console.log("sent an event ");
+        }
+
+
+
+
+
+}
 
