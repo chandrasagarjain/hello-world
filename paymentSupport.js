92dc7001776077223ab31b6b83e5357b56fbda29
chandrasagarjain <chandrasagarjain@gmail.com>
2018-09-18 10:59:41 +0530

    Correct Payment

--- paymentSupport.js
+++ paymentSupport.js
@@ -1,7 +1,10 @@
 var mvp = document.getElementById('myViewport');
-mvp.setAttribute('content','width=device-width,initial-scale=0.4,minimum-scale=0.4,maximum-scale=0.4,user-scalable=no');	mvp.setAttribute('content','width=device-width,initial-scale=0.4,minimum-scale=0.4,maximum-scale=0.4,user-scalable=no');
- var index= document.body.innerText.indexOf("Payment Confirmation Number");                                            	var indexConfirm= document.body.innerText.indexOf("Payment Confirmation Number"); 
-var indexFail= document.body.innerText.indexOf("An error occurred while creating Card transaction: We are unable to process your transaction at this time.");    
- if( index !== -1 ) {	if( indexConfirm !== -1 ) {
-    location.href='https://cuemedocmanager/ConfirmString/;'	    location.href='https://cuemedocmanager/ConfirmString/;'
-}	}
+mvp.setAttribute('content', 'width=device-width,initial-scale=0.4,minimum-scale=0.4,maximum-scale=0.4,user-scalable=no'); mvp.setAttribute('content', 'width=device-width,initial-scale=0.4,minimum-scale=0.4,maximum-scale=0.4,user-scalable=no');
+
+/* To get the status of payment */
+var indexConfirm = document.body.innerText.indexOf("Payment Confirmation Number");
+var indexFail = document.body.innerText.indexOf("An error occurred while creating Card transaction: We are unable to process your transaction at this time.");
+    if (indexConfirm !== -1) {
+        location.href = 'https://cuemedocmanager/bnyString/;'
+    }
+
