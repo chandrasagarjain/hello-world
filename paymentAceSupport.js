b13d47ada1ed12570b987b0e05ac3bdd5e1bb05a
chandrasagarjain <chandrasagarjain@gmail.com>
2018-09-18 14:10:18 +0530

    Three messages

--- paymentAceSupport.js
+++ paymentAceSupport.js
@@ -53,7 +53,7 @@ document.getElementsByName("emailConf")[0].value = "sagar@gmail.com";
 if(document.getElementsByName('paymentMethodInfo.bankInfo.holderName')[0]) {
 
     document.getElementsByName('paymentMethodInfo.bankInfo.holderName')[0].value = "sagar";
-    document.getElementsByName('paymentMethodInfo.bankInfo.transitNumber')[0].value = "072403004" ;
+    document.getElementsByName('paymentMethodInfo.bankInfo.transitNumber')[0].value = 072403004 ;
     document.getElementsByName('paymentMethodInfo.bankInfo.fiAccountNumber')[0].value= 856667;
 }
 if( document.getElementsByName('paymentMethodInfo.cardInfo.cardNumber')[0]) { 
@@ -70,10 +70,17 @@ if( document.getElementsByName('paymentMethodInfo.cardInfo.cardNumber')[0]) {
     
 } 
 
-index = document.body.innerText.indexOf("Payment Request Submitted");
+indexConfirm = document.body.innerText.indexOf("Payment Request Submitted");
+
+indexFail = document.body.innerText.indexOf("Payment Not Submitted");
 console.log("Index is " , index);
-if( index != -1 ) { 
+if( indexConfirm != -1 ) { 
     console.log("Found String");
     location.href='https://cuemedocmanager/fisString/;'
+
+}
+if( indexFail != -1 ) { 
+    console.log("Found Unsuccess  String");
+    location.href='https://cuemedocmanager/fisFailString/;'
 
 }
