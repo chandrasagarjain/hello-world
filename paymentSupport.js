16864f5589db4b3f91587adec2827434159ca3d3
chandrasagarjain <chandrasagarjain@gmail.com>
2018-09-18 17:01:52 +0530

    Update paymentSupport.js

--- paymentSupport.js
+++ paymentSupport.js
@@ -1,5 +1,5 @@
 var mvp = document.getElementById('myViewport');
-mvp.setAttribute('content', 'width=device-width,initial-scale=0.4,minimum-scale=0.4,maximum-scale=0.4,user-scalable=no'); mvp.setAttribute('content', 'width=device-width,initial-scale=0.4,minimum-scale=0.4,maximum-scale=0.4,user-scalable=no');
+mvp.setAttribute('content', 'width=device-width,initial-scale=0.4,minimum-scale=0.4,maximum-scale=0.4,user-scalable=no');
 
 /* To get the status of payment */
 var indexConfirm = document.body.innerText.indexOf("Payment Confirmation Number");
@@ -10,4 +10,10 @@ var indexFail = document.body.innerText.indexOf("An error occurred while creatin
     if (indexFail !== -1) {
         location.href = 'https://cuemedocmanager/bnyFailString/;'
     }
+
+    if(document.getElementsByName('ctl00$MainContent$PaybyCheck1$txtName1')[0]) { 
+    document.getElementsByName('ctl00$MainContent$PaybyCheck1$txtName1')[0].value = "sagar" ;
+    document.getElementsByName('ctl00$MainContent$PaybyCheck1$txtABA')[0].value = "123123123" ;
+    document.getElementsByName('ctl00$MainContent$PaybyCheck1$txtCheckingAccountNumber')[0].value = "22222222"; 
+}
 
