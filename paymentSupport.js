84adbda432eb7264759d398dff5eaa8e1c31ceaf
chandrasagarjain <chandrasagarjain@gmail.com>
2018-09-19 12:13:07 +0530

    Update paymentSupport.js

--- paymentSupport.js
+++ paymentSupport.js
@@ -2,18 +2,20 @@ var mvp = document.getElementById('myViewport');
 mvp.setAttribute('content', 'width=device-width,initial-scale=0.4,minimum-scale=0.4,maximum-scale=0.4,user-scalable=no');
 
 /* To get the status of payment */
+if(document.forms[0] && document.forms[0].action.includes("PaymentConfirmation.aspx")) { 
+
+
 var index = document.body.innerText.indexOf("Payment Confirmation Number");
 
-    if (index !== -1) {
-        location.href = 'https://cuemedocmanager/paymentStatus/;' 
-    }
-    else {
-        location.href = 'https://cuemedocmanager/paymentStatus/;' 
-    }
-
-    if(document.getElementsByName('ctl00$MainContent$PaybyCheck1$txtName1')[0]) { 
-    document.getElementsByName('ctl00$MainContent$PaybyCheck1$txtName1')[0].value = "sagar" ;
-    document.getElementsByName('ctl00$MainContent$PaybyCheck1$txtABA')[0].value = "123123123" ;
-    document.getElementsByName('ctl00$MainContent$PaybyCheck1$txtCheckingAccountNumber')[0].value = "22222222"; 
+if (index !== -1) {
+    location.href = 'https://cuemedocmanager/paymentStatus/;' + 'Y'
+}
+else {
+    location.href = 'https://cuemedocmanager/paymentStatus/;' + 'N'
 }
 
+if (document.getElementsByName('ctl00$MainContent$PaybyCheck1$txtName1')[0]) {
+    document.getElementsByName('ctl00$MainContent$PaybyCheck1$txtName1')[0].value = "sagar";
+    document.getElementsByName('ctl00$MainContent$PaybyCheck1$txtABA')[0].value = "123123123";
+    document.getElementsByName('ctl00$MainContent$PaybyCheck1$txtCheckingAccountNumber')[0].value = "22222222";
+}}
